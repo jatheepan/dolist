c07a94436f7b7cf14eb4c6c6e1c8c84111454a0c
Theepan Kanthavel <theepan@cutedrops.com>
2018-07-05 08:13:00 -0400

    select all functionality implemented

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -14,7 +14,13 @@ const Item = ({ item, onSelectionChange }) => {
   /* eslint-disable jsx-a11y/label-has-for */
   return (
     <li className={classes}>
-      <input type="checkbox" id={`d${item.id}`} onChange={onChange} ref={(input) => { inputCheckbox = input; }} />
+      <input
+        type="checkbox"
+        id={`d${item.id}`}
+        onChange={onChange}
+        checked={item.selected}
+        ref={(input) => { inputCheckbox = input; }}
+      />
       <label htmlFor={`d${item.id}`}>{item.label}</label>
     </li>
   );
@@ -26,8 +32,10 @@ Item.propTypes = {
 };
 
 const List = ({ items, onSelectionChange }) => {
+  let selectAllCheckbox = null;
   const listItems = items.map((item) => {
     const onChange = (_item, _checked) => {
+      selectAllCheckbox.checked = false;
       onSelectionChange([{
         id: _item.id,
         checked: _checked,
@@ -35,8 +43,16 @@ const List = ({ items, onSelectionChange }) => {
     };
     return (<Item key={item.id} item={item} onSelectionChange={onChange} />);
   });
+  const onSelectAllChange = () => {
+    onSelectionChange(items.map(item => ({
+      id: item.id,
+      checked: selectAllCheckbox.checked,
+    })));
+  };
   return (
     <div className="List">
+      <input type="checkbox" id="selectAll" ref={(input) => { selectAllCheckbox = input; }} onChange={onSelectAllChange} />
+      <label htmlFor="selectAll">Select all</label>
       <ul className="list-group list-group-flush">{listItems}</ul>
     </div>
   );
