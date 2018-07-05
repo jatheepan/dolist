35632caebf7ded35a55192250d599e078ede2a48
Theepan Kanthavel <theepan@cutedrops.com>
2018-07-05 06:12:48 -0400

    List Item component seperated

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,26 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const List = ({ items, onSelectionChange }) => {
-  const listItems = items.map((item) => {
-    const classes = classNames('list-group-item', 'list-group-item-action', {
-      active: item.selected,
-      completed: item.done,
-    });
-    let inputCheckbox = null;
-    const onChange = () => {
-      onSelectionChange(item, inputCheckbox.checked);
-    };
-    /* eslint-disable jsx-a11y/label-has-for */
-    return (
-      <li key={item.id} className={classes}>
-        <input type="checkbox" id={`d${item.id}`} onChange={onChange} ref={(input) => { inputCheckbox = input; }} />
-        <label htmlFor={`d${item.id}`}>{item.label}</label>
-      </li>
-    );
-    /* eslint-enable */
+const Item = ({ item, onSelectionChange }) => {
+  const classes = classNames('list-group-item', 'list-group-item-action', {
+    active: item.selected,
+    completed: item.done,
   });
-  return (<ul className="List list-group list-group-flush">{listItems}</ul>);
+  let inputCheckbox = null;
+  const onChange = () => {
+    onSelectionChange(item, inputCheckbox.checked);
+  };
+  /* eslint-disable jsx-a11y/label-has-for */
+  return (
+    <li className={classes}>
+      <input type="checkbox" id={`d${item.id}`} onChange={onChange} ref={(input) => { inputCheckbox = input; }} />
+      <label htmlFor={`d${item.id}`}>{item.label}</label>
+    </li>
+  );
+};
+
+Item.propTypes = {
+  item: PropTypes.objectOf(PropTypes.any).isRequired,
+  onSelectionChange: PropTypes.func.isRequired,
+};
+
+const List = ({ items, onSelectionChange }) => {
+  const listItems = items.map(item => (
+    <Item key={item.id} item={item} onSelectionChange={onSelectionChange} />
+  ));
+  return (
+    <div className="List">
+      <ul className="list-group list-group-flush">{listItems}</ul>
+    </div>
+  );
 };
 
 List.propTypes = {
